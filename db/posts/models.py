9afd2f5a968f75b360e9a8fedee66dad2e3d0918
vitaliiSeptemner <vitaliiseptember@gmail.com>
2024-05-04 16:12:47 +0300

    added Model. rewritten __str__

--- db/posts/models.py
+++ db/posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    """inherited by Django model to describe DB model"""
+    title = models.CharField(max_length=75)
+    body = models.TextField()
+    slug = models.SlugField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
