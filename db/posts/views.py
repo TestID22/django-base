11ec7c8c05db87a9fb67a3155d87eee412886919
vitaliiSeptemner <vitaliiseptember@gmail.com>
2024-05-05 15:16:56 +0300

    view - pure - record in DB fix

--- db/posts/views.py
+++ db/posts/views.py
@@ -10,6 +10,6 @@ def index(request):
         post = Post()
         form = NameForm(request.POST)
         print(form.data)
-        post.post = form.data #TODO: fix incorrect recroding in the DB
+        post.post = form.data['your_task'] #TODO: fix incorrect recroding in the DB
         post.save()
     return  render(request, "posts/index.html", {"form": form }) #dont forget to add FORM for rendering
