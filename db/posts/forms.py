feb6fb99181f67d52c79812977634752011a424f
vitaliiSeptemner <vitaliiseptember@gmail.com>
2024-05-05 14:43:34 +0300

    Created - form for Tasks index - implemented the form posts.views - takes POST from the form

---
+++ db/posts/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+
+class NameForm(forms.Form):
+    your_task = forms.CharField(label='', max_length=100)
--- db/posts/views.py
+++ db/posts/views.py
@@ -1,11 +1,12 @@
+from django.http import HttpResponse
 from django.views.generic import ListView
 from django.shortcuts import render
 from .models import Post
+from .forms import NameForm
 
 def index(request):
-    context = {
-        "title" : "vitalii test",
-        "name" : ['dima', 'alex'],
-        "is_true" : False
-    }
-    return  render(request, "posts/index.html", context)
+    form = NameForm()
+    if request.method == "POST":
+        form = NameForm(request.POST)
+        print(form.data)
+    return  render(request, "posts/index.html", {"form": form }) #dont forget to add FORM for rendering
