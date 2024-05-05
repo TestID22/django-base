6fbf168c9f15c453d3eb0870e398dba5220025f5
vitaliiSeptemner <vitaliiseptember@gmail.com>
2024-05-05 15:14:08 +0300

    model - rewritten model. made DB migration view - implemented relation to DB

--- db/posts/models.py
+++ db/posts/models.py
@@ -2,10 +2,8 @@ from django.db import models
 
 class Post(models.Model):
     """inherited by Django model to describe DB model"""
-    title = models.CharField(max_length=75)
-    body = models.TextField()
-    slug = models.SlugField()
-    date = models.DateTimeField(auto_now_add=True)
+    post = models.CharField(max_length=75)
+
 
     def __str__(self):
-        return self.title
+        return self.post
--- db/posts/views.py
+++ db/posts/views.py
@@ -7,6 +7,9 @@ from .forms import NameForm
 def index(request):
     form = NameForm()
     if request.method == "POST":
+        post = Post()
         form = NameForm(request.POST)
         print(form.data)
+        post.post = form.data #TODO: fix incorrect recroding in the DB
+        post.save()
     return  render(request, "posts/index.html", {"form": form }) #dont forget to add FORM for rendering
