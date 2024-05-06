ace76d12839049fb89f845f452def8dbdc20c62d
vitaliiSeptemner <vitaliiseptember@gmail.com>
2024-05-06 12:45:23 +0300

    html: added output onto the mainpage
    added DB outpiut

--- db/posts/views.py
+++ db/posts/views.py
@@ -1,17 +1,16 @@
 from django.http import HttpResponse
 from django.views.generic import ListView
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
 from .forms import NameForm
 
 def index(request):
     form = NameForm()
     posts = Post.objects.all()
-    print(posts[3])
     if request.method == "POST":
         post = Post()
         form = NameForm(request.POST)
         print(form.data)
         post.post = form.data['your_task'] #TODO: fix incorrect recroding in the DB
         post.save()
-    return  render(request, "posts/index.html", {"form": form }) #dont forget to add FORM for rendering
+    return  render(request, "posts/index.html", {"form": form, "posts": posts }) #dont forget to add FORM for rendering
